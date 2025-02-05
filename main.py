2a51a3d04ae0a3d6a85526d4bc6d9039fecc996b
aramdeveloper1 <aramdeveloper1@gmail.com>
2025-02-05 09:27:40 +0300

    updated word count

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
 def main():
-    # Define the file path
     file_path = "books/frankenstein.txt"
     
-    # Open the file and read its contents
+    # Read the file content
     with open(file_path, "r") as f:
         file_contents = f.read()
     
-    # Print the entire content of the book
-    print(file_contents)
+    # Count words in the file
+    word_count = count_words(file_contents)
+    
+    # Print the total word count
+    print(f"The book contains {word_count} words.")
+    print(f"The book contains {count_characters(file_contents)} characters.")
+    print(f"The book contains {sort_characters(characters)} characters.")
+def count_words(text):
+    """Returns the number of words in the given text."""
+    words = text.split()  # Split text into words using whitespace
+    return len(words)
+
+characters = {}
+def count_characters(text):
+    """Returns the number of characters in the given text."""
+    
+    for char in text:
+        # print("****",char)
+        
+        if char in characters:
+            characters[char] += 1
+            
+        else:
+            characters[char] = 1
+    print("characters count",characters)
+    return len(characters)
+def sort_characters(characters):
+    """Returns the sorted characters in the given text."""
+    sorted_chars = sorted(characters.keys())
+    return sorted_chars
+
 
 # Run the main function
 if __name__ == "__main__":
